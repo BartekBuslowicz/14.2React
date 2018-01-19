3932c053a85c46a0cbc27c07ae6ca202be35a05a
Bartek Buslowicz <bartek@burin.pl>
2018-01-19 12:59:18 +0100

    Add  ready3 project

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc),
-      React.createElement("img", {src: movie.poster}, /*...*/)
+      React.createElement("img", {src: movie.poster},)
     );
 });
 var element =
